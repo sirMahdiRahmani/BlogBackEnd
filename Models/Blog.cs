2933342dbccbe23459c6f9b12507b48666cd96f4
sirmahdi <sir.mahdirhmani@gmail.com>
2024-01-27 17:46:31 +0330

    Changing Models

--- Models/Blog.cs
+++ Models/Blog.cs
@@ -1,44 +1,60 @@
+using Microsoft.AspNetCore.Identity;
+
 namespace BlogBackEnd.Models;
 
-
-public class User
+public class User : IdentityUser
 {
-    public int UserId { get; set; }
-    public string? UserName { get; set; }
-    public ICollection<Post> Posts { get; set; } = new List<Post>();
+    // You can add custom properties here if needed
 }
 
 public class Post
 {
     public int PostId { get; set; }
-    public string? Title { get; set; }
-    public string? Content { get; set; }
-    public int UserId { get; set; }
-    public User? User { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public Image Image { get; set; }
+
+    public string UserId { get; set; }
+    public User User { get; set; }
+
     public ICollection<Comment> Comments { get; set; } = new List<Comment>();
-    public ICollection<Tag> Tags { get; set; } = new List<Tag>();
-    public Image? Image { get; set; }
+    public ICollection<PostTag> PostTags { get; set; }
 }
 
 public class Comment
 {
     public int CommentId { get; set; }
     public string? Content { get; set; }
+    public DateTime CreatedAt { get; set; }
+    
+    public int UserId { get; set; }
+    public User User { get; set; }
+    
     public int PostId { get; set; }
-    public Post? Post { get; set; }
+    public Post Post { get; set; }
 }
 
 public class Tag
 {
     public int TagId { get; set; }
     public string? Name { get; set; }
-    public ICollection<Post> Posts { get; set; } = new List<Post>();
+    public ICollection<PostTag> Posts { get; set; }
+}
+
+public class PostTag
+{
+    public int PostId { get; set; }
+    public Post Post { get; set; }
+    
+    public int TagId { get; set; }
+    public Tag Tag { get; set; }
 }
 
 public class Image
 {
     public int ImageId { get; set; }
-    public string? Url { get; set; }
+    public string Url { get; set; }
+    
     public int PostId { get; set; }
     public Post? Post { get; set; }
 }
